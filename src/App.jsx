4c073653d388c7216630ff89a2b31c62ed1ee500
Karya Lingga <usscharlesberry@gmail.com>
2025-05-30 21:43:08 +0700

    useEffect for local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Login from "./pages/Login";
 import Quiz from "./pages/Quiz";
 import Result from "./pages/Result";
-// import { getSavedUser } from "./utils/localStorage";
+import { getSavedUser } from "./utils/localStorage";
 
 function App() {
   const [user, setUser] = useState(null);
   const [quizComplete, setQuizComplete] = useState(false);
   const [result, setResult] = useState(null);
 
-  // useEffect(() => {
-  //   const saved = getSavedUser();
-  //   if (saved) setUser(saved);
-  // }, []);
+  useEffect(() => {
+    const saved = getSavedUser();
+    if (saved) setUser(saved);
+  }, []);
 
   if (!user) return <Login onLogin={setUser} />;
   if (quizComplete) return <Result result={result} />;
